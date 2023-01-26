7a9f7c3e1893657a6b9033988328219acd78bc64
andre-m2y <jose.moreira@mobile2you.com.br>
2023-01-26 12:36:35 -0300

    add gem rubycritic and simplecov

--- test/controllers/imc_test.rb
+++ test/controllers/imc_test.rb
@@ -16,17 +16,17 @@ class ImcTest < ActionDispatch::IntegrationTest
   test 'should respond with 400 when the params are missing' do
     post '/imc', headers: { 'HTTP_AUTHORIZATION' => 'Bearer ' + build_jwt },
                  params: { height: 1.70, weight: 76 }
-                 assert_response 400
-                 
-                 assert_equal(
-                   { 'error' => 'param is missing or the value is empty: imc' },
-                   JSON.parse(response.body)
-                  )
-                end
-                
-                test 'should respond with 422 when receives invalid params' do
-                  post '/imc', headers: { 'HTTP_AUTHORIZATION' => 'Bearer ' + build_jwt },
-                  params: { imc: { height: '' } }
+    assert_response 400
+
+    assert_equal(
+      { 'error' => 'param is missing or the value is empty: imc' },
+      JSON.parse(response.body)
+    )
+  end
+
+  test 'should respond with 422 when receives invalid params' do
+    post '/imc', headers: { 'HTTP_AUTHORIZATION' => 'Bearer ' + build_jwt },
+                 params: { imc: { height: '' } }
 
     assert_response 422
 
@@ -50,7 +50,7 @@ class ImcTest < ActionDispatch::IntegrationTest
 
   test 'should respond with success when receives valid params' do
     post '/imc', headers: { 'HTTP_AUTHORIZATION' => 'Bearer ' + build_jwt },
-                  params: { imc: { height: 1.85, weight: 85 } }
+                 params: { imc: { height: 1.85, weight: 85 } }
 
     assert_response 200
 
@@ -71,10 +71,10 @@ class ImcTest < ActionDispatch::IntegrationTest
   end
 
   def build_jwt(valid_for_minutes = 5)
-    exp = Time.now.to_i + (valid_for_minutes*60)
-    payload = { "iss": "fusionauth.io",
+    exp = Time.now.to_i + (valid_for_minutes * 60)
+    payload = { "iss": 'fusionauth.io',
                 "exp": exp,
-                "aud": "238d4793-70de-4183-9707-48ed8ecd19d9",
+                "aud": '238d4793-70de-4183-9707-48ed8ecd19d9',
                 "sub": '19016b73-3ffa-4b26-80d8-aa9287738677',
                 "name": 'Dan Moore',
                 "roles": ['USER'] }
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,26 @@
-ENV["RAILS_ENV"] ||= "test"
-require_relative "../config/environment"
-require "rails/test_help"
+require 'simplecov'
+
+SimpleCov.start 'rails' do
+  enable_coverage :branch
+end
+
+ENV['RAILS_ENV'] ||= 'test'
+require_relative '../config/environment'
+require 'rails/test_help'
+
+Rails.root.join('test/support').then do |pathname|
+  require pathname.join('regexp_patterns')
+  require pathname.join('hash_schema_assertions')
+  require pathname.join('todo_assertions')
+end
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+  # parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  include HashSchemaAssertions
 end
