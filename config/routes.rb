e41bafbe463ac0e22e1bbdb5daeb920c89fbfad6
andre-m2y <jose.moreira@mobile2you.com.br>
2023-01-26 11:31:17 -0300

    config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post '/imc' => 'imc#imc'
+  get '/get_token' => 'imc#get_token'
 end
